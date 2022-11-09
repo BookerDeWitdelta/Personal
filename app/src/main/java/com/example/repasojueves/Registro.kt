1d075326fa27a2359404604b68d287109c8ab1f6
BookerDeWitdelta <andreyandreygarciazapata@gmail.com>
2022-11-09 08:13:11 -0500

    Nuevo repositorio?

--- app/src/main/java/com/example/repasojueves/Registro.kt
+++ app/src/main/java/com/example/repasojueves/Registro.kt
@@ -5,13 +5,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.room.Room
+import androidx.room.Room.Companion.databaseBuilder
 import com.example.repasojueves.databinding.ActivityFotoBinding
 import com.example.repasojueves.databinding.ActivityRegistroBinding
+import com.example.repasojueves.model.UsuarioModdel
 
 class Registro : AppCompatActivity() {
     private lateinit var binding:ActivityRegistroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        database= databaseBuilder(application,UsuarioBD::class.java,UsuarioDB.DATABASE_NAME).allowMainThreedQueries().build()
         binding=ActivityRegistroBinding.inflate(layoutInflater)
         val view=binding.root
         setContentView(view)
@@ -19,7 +23,8 @@ class Registro : AppCompatActivity() {
             startActivity(Intent(this,Foto::class.java))
         }
         binding.btnregistrarusuario.setOnClickListener{
-            guardar()
+            //guardar()
+            guardarconroom()
         }
     }
     fun guardar(){
@@ -39,5 +44,15 @@ class Registro : AppCompatActivity() {
         editor.putString("clave",txtclave)
         editor.commit()
         Toast.makeText(this,"Datos guardados",Toast.LENGTH_LONG).show()
+    }
+    fun guardarconroom(){
+        val txtnombre=binding.nombre.text.toString()
+        val txtapellido=binding.apellido.text.toString()
+        val txtcorreo=binding.correo.text.toString()
+        val txtcelular=binding.celular.text.toString()
+        val txtusuario=binding.usuario.text.toString()
+        val txtclave=binding.clave.text.toString()
+        val usu= UsuarioModdel(txtusuario,txtnombre,txtapellido,txtcorreo,txtcelular,txtclave)
+        database.usuarioDao.insertar(usu)
     }
 }
