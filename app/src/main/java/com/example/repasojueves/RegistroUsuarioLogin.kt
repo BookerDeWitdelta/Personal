5ee881fcddab000cf76aae7fd60a6e06ab313e62
BookerDeWitdelta <andreyandreygarciazapata@gmail.com>
2022-11-10 07:28:33 -0500

    Repositorio personal 2.0

---
+++ app/src/main/java/com/example/repasojueves/RegistroUsuarioLogin.kt
@@ -0,0 +1,11 @@
+package com.example.repasojueves
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroUsuarioLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_usuario_login)
+    }
+}
